77d7f4c2784309176c25cd3360f150468c465793
Urimubenshi Daniel <benshidanny11@gmail.com>
2019-04-19 18:46:48 +0200

    Added loan feature ui

--- UI/js/loan.js
+++ UI/js/loan.js
@@ -1,10 +1,5 @@
-const infoDiv=document.getElementById("info");
-const loanDiv=document.getElementById("loan-div");
 
-function callLoanFOrm(){
-    infoDiv.style.display="none";
-    loanDiv.style.display="flex";
-}
+
 function hideLoanForm(){
-    document.location.reload();
+    window.location.href = 'dashboard.html';
 }
