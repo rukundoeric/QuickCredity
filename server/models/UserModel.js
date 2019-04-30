b2de505de1c4f0a5a0f0f73740381511f7bf01d0
Urimubenshi Daniel <benshidanny11@gmail.com>
2019-04-30 14:46:22 +0200

    [Feature #165609464] Added user signup api

---
+++ server/models/UserModel.js
@@ -0,0 +1,58 @@
+
+class User {
+  constructor() {
+    this.userData = [
+      {
+        id: 1,
+        firstName: 'Urimubenshi',
+        lastName: 'Daniel',
+        email: 'benshidanny11@gmail.com',
+        address: 'Kimironko',
+        userName: 'benshidanny11',
+        createdOn: '4/28/2019',
+        userRole: 'client',
+        password: 'danny123',
+        status: 'Unverified',
+      },
+      {
+        id: 2,
+        firstName: 'Dukuze',
+        lastName: 'Emmy',
+        email: 'dukuze11@gmail.com',
+        address: 'Kimironko',
+        userName: 'dukuze11',
+        createdOn: '4/28/2019',
+        userRole: 'admin',
+        password: 'emmy123',
+        status: 'verified',
+      },
+    ];
+  }
+
+  async signup(user) {
+    if (!user) {
+      return false;
+    }
+    this.userData.push(user);
+    return true;
+  }
+
+  async getUserByEmail(email) {
+    return this.userData.find(user => user.email === email);
+  }
+
+  async getUserByName(userName) {
+    const user = this.userData.find(user => user.userName === userName);
+    return !user ? null : user.userName;
+  }
+
+  async getUserById(id) {
+    return this.userData.find(user => user.id === id);
+  }
+
+  async getAllUsers() {
+    return this.userData;
+  }
+}
+
+export default new User();
